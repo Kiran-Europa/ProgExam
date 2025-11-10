e1d3dd3fddf3974ecda52c8e64eaab2653ffe9aa
Kiran Europa <kceuropa1@gmail.com>
2025-11-10 12:51:19 +0200

    Add files via upload

---
+++ Q2iProductSales.java
@@ -0,0 +1,9 @@
+ public interface iProductSales {
+        int[][] GetProductsales();
+        int GetTotalSales();
+        int GetSalesOverLimit();
+        int GetSalesUnderLimit();
+        int GetProductsProcessed();
+        double GetAverageSales();
+    }
+
